3c7eb7f40203a0aabbd848d7e9b238cd7b7fc516
Gus Class <gguuss@gmail.com>
2014-07-22 06:33:23 +0100

    Initial app

---
+++ GoogleDotNetDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using Google.Apis.Oauth2.v2;
+using Google.Apis.Oauth2.v2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleDotNetDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(@"Input an Access token:");
+            String accessToken = Console.ReadLine();
+
+            Oauth2Service service = new Oauth2Service(
+                new Google.Apis.Services.BaseClientService.Initializer());
+            Oauth2Service.TokeninfoRequest request = service.Tokeninfo();
+            request.AccessToken = accessToken;
+
+            Tokeninfo info = request.Execute();
+            Console.Write(@"Scope: " + info.Scope + "\n");
+            Console.WriteLine(@"Expires: " + info.ExpiresIn);
+        }
+    }
+}
